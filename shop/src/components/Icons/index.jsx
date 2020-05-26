46b9759b6fc3513784f9a226d2acfed97a3d7d09
xueqing618 <3419590244@qq.com>
2020-05-26 23:08:00 +0800

    布局

--- shop/src/components/Icons/index.jsx
+++ shop/src/components/Icons/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Icon } from 'antd';
 
 const IconFont = Icon.createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1776885_4tgwujnc1cl.js',
+  scriptUrl: '//at.alicdn.com/t/font_1776885_9wnl31u3yn7.js',
 });
 export default class Icons extends Component {
   render() {
