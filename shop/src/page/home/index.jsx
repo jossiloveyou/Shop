b00624fb9aeb115dca93f13671f9d6032f64472e
xueqing618 <3419590244@qq.com>
2020-05-29 20:45:11 +0800

    icons

--- shop/src/page/home/index.jsx
+++ shop/src/page/home/index.jsx
@@ -3,12 +3,6 @@ import React, { Component } from 'react'
 import { Swiper } from '@@'
 
 import HomeHeader from '@/page/home_header'
-<<<<<<< HEAD
-import Navs from '@/page/home_navs'
-import Recommend from '@/page/home_recommend'
-import Swiper from '@/components/Swiper'
-=======
->>>>>>> ba0ea0d5a82ec93de5670a8d0c4186c2d8cd410c
 import { post } from '@/utils/request'
 import api from '@/services/api'
 import './styles.less'
@@ -16,7 +10,8 @@ import './styles.less'
 export default class Home extends Component {
   state={
     bannerData:[],
-    navs:[],
+    datas:[],
+    data:[],
   }
   componentDidMount () {
     post(api.swipe).then(res => {
@@ -24,26 +19,15 @@ export default class Home extends Component {
         bannerData: res.data
       })
     })
-
-    post(api.navs).then(res => {
-      this.setState({
-        navs: res.data
-      })
-    })
   }
   render() {
-    let { bannerData, navs } = this.state
-    console.log(bannerData)
+    let { bannerData, datas, data} = this.state
     return (
       <div className="home-box">
         <HomeHeader />
         {
           bannerData.length ? <Swiper data={bannerData} /> : null
         }
-        {
-          navs.length ? <Navs navs={navs}></Navs> : undefined
-        }
-        <Recommend />
       </div>
     )
   }
