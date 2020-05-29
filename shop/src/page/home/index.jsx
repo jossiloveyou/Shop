0560675f9ecbb2ab7c172ce6d61a981002ff190d
EquinoxFlowerFlow <xxzy1010@163.com>
2020-05-29 20:45:50 +0800

    '111'

--- shop/src/page/home/index.jsx
+++ shop/src/page/home/index.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
-
 import { Swiper } from '@@'
-
 import HomeHeader from '@/page/home_header'
-<<<<<<< HEAD
 import Navs from '@/page/home_navs'
 import Recommend from '@/page/home_recommend'
-import Swiper from '@/components/Swiper'
-=======
->>>>>>> ba0ea0d5a82ec93de5670a8d0c4186c2d8cd410c
+// import Swiperr from '@/components/Swiper'
 import { post } from '@/utils/request'
 import api from '@/services/api'
 import './styles.less'
